7eb71812c66545fc70af854b227e5149793b5807
senx <zhou691226@qq.com>
2023-10-21 11:50:05 +0800

    First Upload

---
+++ src/main/java/com/huashan/jianfa/dgjj.java
@@ -0,0 +1,11 @@
+package com.huashan.jianfa;
+
+public class dgjj {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+    }
+}
